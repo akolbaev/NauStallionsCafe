8a31e9d61bc9052983264c4af5a372ea8e515e7f
ikamilov <kamilov6@gmail.com>
2017-04-11 21:22:59 -0500

    changed to php

---
+++ includes/inc_bulletin_board.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="row">
+    <div class="col-xs-12">
+        <ul class="breadcrumb">
+            <li><a href="index.php?page=home_page">Home</a></li>
+            <li class="active">Bulletin Board</li>
+        </ul>
+    </div>            
+</div>
+ <div class="container " >
+               <h1 style="text-align: center;">Bulletin Board</h1>
+               <hr>
+            </div>
+        </div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div>
+                        <h2 style="text-align: center;">Selling</h2>
+                        <div>
+                            <p style="padding:10px;"></p>
+                        </div>
+                         <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a title="Image 1" href="#" target="">
+                                    <img src="images/bike.jpg" alt="bike" ">
+                                    <div class="caption">
+                                        <p>Price: $80 USD</p>
+                                        <p>Bike</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/iphone.jpg"  alt="iphone" ">
+                                    <div class="caption">
+                                        <p>Price: $250 USD</p>
+                                        <p>Iphone 6S</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/table.jpg" style="" alt="table" ">
+                                    <div class="caption">
+                                        <p>Price: $45 USD</p>
+                                        <p>Study Table</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/collegealgebra.jpg" style="" alt="collegealgebra" ">
+                                    <div class="caption">
+                                        <p>Price: $35 USD</p>
+                                        <p>College Algebra</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/table.jpg" style="" alt="table" ">
+                                    <div class="caption">
+                                        <p>Price: $45 USD</p>
+                                        <p>Study Table</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/collegealgebra.jpg" style="" alt="collegealgebra" ">
+                                    <div class="caption">
+                                        <p>Price: $35 USD</p>
+                                        <p>College Algebra</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div style="text-align: center;">
+                            <a class="btn btn-primary" href="index.php?page=bulletin_selling">View More . . .</a>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <p style="padding:20px;"></p>
+                    </div>
+                
+                </div>
+                <div class="col-md-4">
+                    <div>
+                        <h2 style="text-align: center;">Buying</h2>
+                        <div>
+                            <p style="padding:10px;"></p>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a title="Image 1" href="#" target="">
+                                    <img src="images/red.jpg" alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $46 USD</p>
+                                        <p>Bike</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/blue.jpg"  alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $250 USD</p>
+                                        <p>Iphone 6S</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/blue.jpg" style="" alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $32 USD</p>
+                                        <p>Study Table</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/red.jpg" style="" alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $65 USD</p>
+                                        <p>College Algebra</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/red.jpg" style="" alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $50 USD</p>
+                                        <p>Study Table</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/blue.jpg" style="" alt="collegealgebra" ">
+                                    <div class="caption">
+                                        <p>Price: $35 USD</p>
+                                        <p>College Algebra</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div style="text-align: center;">
+                            <a class="btn btn-primary" href="index.php?page=bulletin_buying">View More . . .</a>
+                        </div>
+                    </div>
+                    <div>
+                        <p style="padding:20px;"></p>
+                    </div>
+                
+                </div> 
+                <div class="col-md-4">
+                    <div>
+                        <h2 style="text-align: center;">Living</h2>
+                        <div>
+                            <p style="padding:10px;"></p>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a title="Image 1" href="#" target="">
+                                    <img src="images/red.jpg" alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $46 USD</p>
+                                        <p>Bike</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/blue.jpg"  alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $250 USD</p>
+                                        <p>Iphone 6S</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/blue.jpg" style="" alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $32 USD</p>
+                                        <p>Study Table</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/red.jpg" style="" alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $65 USD</p>
+                                        <p>College Algebra</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/red.jpg" style="" alt="" ">
+                                    <div class="caption">
+                                        <p>Price: $50 USD</p>
+                                        <p>Study Table</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="thumbnail">
+                                <a href="#" target="">
+                                    <img src="images/blue.jpg" style="" alt="collegealgebra" ">
+                                    <div class="caption">
+                                        <p>Price: $35 USD</p>
+                                        <p>College Algebra</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div style="text-align: center;">
+                            <a class="btn btn-primary" href="index.php?page=bulletin_living">View More . . .</a>
+                        </div>
+
+                    </div>
+                    <div>
+                        <p style="padding:20px;"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div tabindex="-1" class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button class="close" type="button" data-dismiss="modal">×</button>
+                    <h3 class="modal-title">Heading</h3>
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
