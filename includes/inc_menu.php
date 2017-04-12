90cd726dd8ddaf3995ba444ea7c69e00027e73ca
ikamilov <kamilov6@gmail.com>
2017-04-12 14:11:37 -0500

    menu page fixed

--- includes/inc_menu.php
+++ includes/inc_menu.php
@@ -6,22 +6,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head
          content must come *after* these tags -->
     <title></title>
-        <!-- Bootstrap -->
-    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../bower_components/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
-    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="styles/bootstrap-social.css" rel="stylesheet">
-    <link href="styles/mystyles.css" rel="stylesheet">
-    
-    
-    
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+   
 </head>
  
 <body>
@@ -82,7 +67,7 @@
 
 
 
-<script src="../bower_components/angular/angular.min.js"></script>
+<script src="bower_components/angular/angular.min.js"></script>
     <script>
 
         var app = angular.module('Project1',[]);
