b8ab96bff2436d70ea6d2990449932903dea35a8
ikamilov <kamilov6@gmail.com>
2017-04-12 14:21:10 -0500

    baike sizdikindey

--- includes/inc_menu.php
+++ includes/inc_menu.php
@@ -6,9 +6,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head
          content must come *after* these tags -->
     <title></title>
-<<<<<<< HEAD
-   
-=======
+
         <!-- Bootstrap -->
     <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../bower_components/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
@@ -25,7 +23,6 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
->>>>>>> 7b33498021868d34ecfd36858d28a60839263535
 </head>
  
 <body>
