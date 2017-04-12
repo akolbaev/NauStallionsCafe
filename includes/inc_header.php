259eaf09ee81cae0017848c202e5f194e3264d21
ikamilov <kamilov6@gmail.com>
2017-04-11 21:24:48 -0500

    changed to php

---
+++ includes/inc_header.php
@@ -0,0 +1,131 @@
+ 
+ <!DOCTYPE html>
+ <html>
+ <head>
+     <title></title>
+ </head>
+ <body>
+ <!--  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">-->
+          
+   
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+
+                   <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                       <a class="navbar-brand" href="#"><img src="img/img/logo.png" height=30 width=37>  </a>
+
+              
+
+                </div>
+                
+                <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                     <li class="active"><a href = "index.php?page=home_page"><span class="glyphicon glyphicon-home"
+                         aria-hidden="true"></span> Home</a></li>
+                   <!-- <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+                         role="button" aria-haspopup="true" aria-expanded="false">
+                             <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+                         Menu <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Appetizers</a></li>
+                            <li><a href="#">Main Courses</a></li>
+                            <li><a href="#">Desserts</a></li>
+                            <li><a href="#">Drinks</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li class="dropdown-header">Specials</li>
+                            <li><a href="#">Weekend Breakfast</a></li>
+                            
+                        </ul>
+                     </li>-->
+                     <li><a href = "index.php?page=menu"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Menu</a></li>
+                    <li><a href="index.php?page=bulletin_board">Bulletin Board</a></li>
+                    
+                    <li><a href="index.php?page=contact_us">
+                        <i class="fa fa-envelope-o"></i>
+                        Contact Us</a></li>
+                </ul>
+                 <ul class="nav navbar-nav navbar-right">
+                   <!-- <li><a data-toggle="modal" data-target="#loginModal">-->
+                     <li class="active"><a href="index.php?page=login_page"><span class="glyphicon glyphicon-log-in"
+                         aria-hidden="true"></span> Sign in</a></li>
+                    
+                    
+                </ul>
+                
+                </div>
+                
+              
+            </div>
+         
+         
+        </nav>   
+           <!-- Modal content
+          <div id="loginModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+        <!-- Modal content
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Login </h4>
+            </div>
+            <div class="modal-body">
+                <form class="form-inline"> 
+                    <div class="form-group"> 
+                        <label class="sr-only" for="email">Email address</label>
+                        <input type="email" class="form-control input-sm" id="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="password">Password</label>
+                        <input type="password" class="form-control input-sm" id="passwor" placeholder="password">
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox">Remember me
+                        </label>
+                    </div>
+                    <button type="submit" class="btn btn-info btn-sm">Sign in</button>
+                      <button type="button" class="btn btn-default btn-sm"
+                       data-dismiss="modal">Cancel</button>
+                </form>
+
+             
+
+            </div>
+        </div>
+        </div>
+    </div> -->  
+            
+            
+    <header class="jumbotron">
+
+        <!-- Main component for a primary marketing message or call to action -->
+
+        <div class="container">
+            <div class="row row-header">
+                <div class =" col-xs-6 col-sm-5">
+                    <p style="padding:0px;"></p>
+                  
+                     <img src="img/img/logo.png" class="img-reponsive">
+                    <!--<p style="padding:0px;"></p> -->
+                  
+                  
+                </div>
+                
+        
+            </div>
+        </div>
+    </header>
+          
+
+ 
+ </body>
+ </html>
+ 
